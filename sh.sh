f6bb59e7578fe8f6c7abdcd218d2bd66aa527c03
name <75123516+1qe@users.noreply.github.com>
2021-05-01 20:19:42 +0800

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,13 +1,11 @@
-apt-get update
+apt-get update >/dev/null
 apt-get install -y openssh-server jupyter python3-pip git >/dev/null
 
 jupyter-notebook --generate-config >/dev/null
 mv jupyter_notebook_config.py .jupyter/
 
-#git
-echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
-
 echo root:123456 | chpasswd
+echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 chmod +x natfrp
 sed -i s@/archive.ubuntu.com/@/mirrors.ustc.edu.cn/ubuntu/@g /etc/apt/sources.list
