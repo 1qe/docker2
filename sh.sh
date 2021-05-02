b10896cfe679c329201a8c197230e2c1b2d5e210
name <75123516+1qe@users.noreply.github.com>
2021-05-02 20:20:20 +0800

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,17 +1,3 @@
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y openssh-server jupyter python3-pip git
-
-jupyter-notebook --generate-config
-mv jupyter_notebook_config.py .jupyter/
-
-git config --global user.name "191840239"
-git config --global user.email "191840239@smail.nju.edu.cn"
-git remote remove origin
-git remote add origin git@git.nju.edu.cn:191840239/docker2f.git
-
-echo root:123456 | chpasswd
-echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-sed -i s@/archive.ubuntu.com/@/mirrors.ustc.edu.cn/ubuntu/@g /etc/apt/sources.list
-sed -i s@/security.ubuntu.com/@/mirrors.ustc.edu.cn/ubuntu/@g /etc/apt/sources.list
