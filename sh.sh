aeca9a192b992e16f595252694824ed2f050675f
name <75123516+1qe@users.noreply.github.com>
2021-05-08 19:39:14 +0800

    Create sh.sh

--- sh.sh
+++ sh.sh
@@ -1,3 +1,7 @@
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
-apt-get install -y openssh-server jupyter python3-pip git
+apt-get install -y openssh-server jupyter python3-pip git npm
+npm install -g n
+n lts
+PATH=$PATH
+npm install -g npm@latest
